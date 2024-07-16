78be89841d94d1865370b86f98a4d78079a0f56c
Matheus Emanoel <buss.matheusemanoel@gmail.com>
2024-07-15 23:56:40 -0300

    cleanup: remove console.log

--- src/Discord/Class/EventsHandler.ts
+++ src/Discord/Class/EventsHandler.ts
@@ -104,12 +104,9 @@ export class EventsHandler {
       if (message.channel.isThread() && message.channel.invitable === false) {
         const chat = chatHandler.getChat(message.channel.id);
 
-        console.log("test");
         if (!chat) return;
 
-        console.log("test");
         if (message.content === "!stop") {
-          console.log("test");
           if (!(message.author.id === "253195593130835968")) return;
           chat.stopChat();
 
