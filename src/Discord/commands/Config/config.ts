258b95096b65ab872fe2078e3f3695f3499b650f
Matheus Emanoel <buss.matheusemanoel@gmail.com>
2025-01-03 01:18:49 -0300

    refactor: separate the config logic

--- src/Discord/commands/Config/config.ts
+++ src/Discord/commands/Config/config.ts
@@ -1,13 +1,23 @@
 import {
   ActionRowBuilder,
   ButtonBuilder,
+  type ButtonInteraction,
   ButtonStyle,
+  type CacheType,
+  type ChannelSelectMenuInteraction,
   type ChatInputCommandInteraction,
+  ComponentType,
+  type InteractionCollector,
   PermissionFlagsBits,
   PermissionsBitField,
+  type RoleSelectMenuInteraction,
   SlashCommandBuilder,
 } from "discord.js";
 import { responseEmbed } from "../../components/response/index.js";
+import { CreatePannelComponent } from "../../components/createPannelChat/index.js";
+import { ConfigAllowedChannelsComponent } from "../../components/configAllowedChannels/index.js";
+import { ConfigAllowedRolesComponent } from "../../components/configAllowedRoles/index.js";
+import { verifyIfMemberHasAdmPermission } from "../../utils/verifyIfMemberHasAdmPermission.js";
 
 const command = {
   data: new SlashCommandBuilder()
@@ -15,13 +25,9 @@ const command = {
     .setDescription("Configure a Deborah")
     .setContexts(0),
   execute: async (interaction: ChatInputCommandInteraction) => {
-    if (
-      interaction.inGuild() &&
-      interaction.member.permissions instanceof PermissionsBitField &&
-      !interaction.member.permissions.has(PermissionFlagsBits.Administrator)
-    ) {
-      console.log(interaction.member.permissions);
-      console.log("test");
+    const permission = verifyIfMemberHasAdmPermission(interaction);
+    console.log(permission);
+    if (!permission) {
       return interaction.reply({
         content: "Você não tem permissão para usar este comando",
         ephemeral: true,
@@ -33,17 +39,17 @@ const command = {
     );
 
     const channelsAllowedButton = new ButtonBuilder()
-      .setCustomId("allowed-channels-config-button")
+      .setCustomId("config-allowed-channels-button")
       .setLabel("Configurar canais permitidos")
       .setStyle(ButtonStyle.Primary);
 
     const rolesAllowedButton = new ButtonBuilder()
-      .setCustomId("allowed-roles-config-button")
+      .setCustomId("config-allowed-roles-button")
       .setLabel("Configurar cargos permitidos")
       .setStyle(ButtonStyle.Primary);
 
     const createChatPannelButton = new ButtonBuilder()
-      .setCustomId("create-chat-pannel-config-button")
+      .setCustomId("create-chat-pannel-button")
       .setLabel("Criar painel de Chat")
       .setStyle(ButtonStyle.Primary);
 
@@ -53,9 +59,152 @@ const command = {
       createChatPannelButton
     );
 
-    await interaction.reply({
+    const message = await interaction.reply({
       embeds: [response],
       components: [buttons],
+    });
+
+    const buttonCollector = message.createMessageComponentCollector({
+      componentType: ComponentType.Button,
+      filter: verifyIfMemberHasAdmPermission,
+    });
+
+    let configAllowedButtonCollector: InteractionCollector<ButtonInteraction>;
+    let selectChannelCollector: InteractionCollector<ChannelSelectMenuInteraction>;
+    let selectRoleCollector: InteractionCollector<RoleSelectMenuInteraction>;
+
+    buttonCollector.on("collect", async (interaction) => {
+      switch (interaction.customId) {
+        case "create-chat-pannel-button": {
+          interaction.deferUpdate();
+          const createPannelComponent = new CreatePannelComponent(interaction);
+
+          configAllowedButtonCollector =
+            message.createMessageComponentCollector({
+              componentType: ComponentType.Button,
+              filter: verifyIfMemberHasAdmPermission,
+            });
+
+          selectChannelCollector =
+            interaction.message.createMessageComponentCollector({
+              componentType: ComponentType.ChannelSelect,
+              filter: verifyIfMemberHasAdmPermission,
+            });
+
+          selectChannelCollector.on("collect", async (interaction) => {
+            interaction.deferUpdate();
+
+            createPannelComponent.editChannelSelected(interaction);
+          });
+
+          configAllowedButtonCollector.on("collect", (interaction) => {
+            if (interaction.customId === "back-to-home-button") return;
+            interaction.deferUpdate();
+
+            createPannelComponent.createPannel(interaction);
+          });
+          break;
+        }
+
+        case "config-allowed-roles-button": {
+          interaction.deferUpdate();
+          const configAllowedRoles = new ConfigAllowedRolesComponent(
+            interaction
+          );
+
+          configAllowedButtonCollector =
+            interaction.message.createMessageComponentCollector({
+              componentType: ComponentType.Button,
+              filter: verifyIfMemberHasAdmPermission,
+            });
+
+          selectRoleCollector =
+            interaction.message.createMessageComponentCollector({
+              componentType: ComponentType.RoleSelect,
+              filter: verifyIfMemberHasAdmPermission,
+            });
+
+          selectRoleCollector.on("collect", async (interaction) => {
+            interaction.deferUpdate();
+            configAllowedRoles.editAllowedRole(interaction);
+          });
+
+          configAllowedButtonCollector.on("collect", (interaction) => {
+            switch (interaction.customId) {
+              case "add-allowed-role-button":
+                configAllowedRoles.addAllowedRole(interaction);
+                interaction.deferUpdate();
+                break;
+              case "remove-role-from-list-button":
+                configAllowedRoles.removeAllowedRole(interaction);
+                interaction.deferUpdate();
+                break;
+            }
+          });
+
+          break;
+        }
+
+        case "config-allowed-channels-button": {
+          if (!interaction.guild) return;
+          interaction.deferUpdate();
+
+          const configAllowedChannels = new ConfigAllowedChannelsComponent(
+            interaction
+          );
+
+          configAllowedButtonCollector =
+            interaction.message.createMessageComponentCollector({
+              componentType: ComponentType.Button,
+              filter: verifyIfMemberHasAdmPermission,
+            });
+
+          selectChannelCollector =
+            interaction.message.createMessageComponentCollector({
+              componentType: ComponentType.ChannelSelect,
+              filter: verifyIfMemberHasAdmPermission,
+            });
+
+          selectChannelCollector.on("collect", (interaction) => {
+            interaction.deferUpdate();
+            configAllowedChannels.editAllowedChannel(interaction);
+          });
+
+          configAllowedButtonCollector.on("collect", (interaction) => {
+            switch (interaction.customId) {
+              case "add-allowed-channel-button":
+                interaction.deferUpdate();
+                configAllowedChannels.addAllowedChannel(interaction);
+                break;
+              case "remove-channel-from-list-button":
+                configAllowedChannels.removeAllowedChannel(interaction);
+                interaction.deferUpdate();
+                break;
+            }
+          });
+
+          break;
+        }
+
+        case "back-to-home-button":
+          // Stop old collectors
+          if (selectRoleCollector) {
+            selectRoleCollector.stop();
+          }
+
+          if (selectChannelCollector) {
+            selectChannelCollector.stop();
+          }
+
+          configAllowedButtonCollector.stop();
+
+          interaction.deferUpdate();
+
+          interaction.message.edit({
+            embeds: [response],
+            components: [buttons],
+          });
+      }
     });
   },
 };
---
+++ src/Discord/components/createPannelChat/index.ts
@@ -0,0 +1,104 @@
+import {
+  ActionRowBuilder,
+  ButtonBuilder,
+  type ButtonInteraction,
+  ButtonStyle,
+  ChannelSelectMenuBuilder,
+  type ChannelSelectMenuInteraction,
+  ChannelType,
+  type EmbedBuilder,
+} from "discord.js";
+import { responseEmbed } from "../response/index.js";
+
+export class CreatePannelComponent {
+  private channelSelected?: string;
+  private selectRow?: ActionRowBuilder<ChannelSelectMenuBuilder>;
+  private buttonRow?: ActionRowBuilder<ButtonBuilder>;
+  private embed?: EmbedBuilder;
+
+  constructor(interaction: ButtonInteraction) {
+    this.init(interaction);
+  }
+
+  async init(interaction: ButtonInteraction) {
+    this.embed = responseEmbed(
+      "Configuração de IA Chats",
+      "Selecione o canal onde as IA Chats serão criadas\n\nCanal selecionado: `Não selecionado`"
+    );
+
+    const selectChannel = new ChannelSelectMenuBuilder()
+      .setCustomId("select-channels")
+      .setPlaceholder("Selecione o canal")
+      .setChannelTypes(ChannelType.GuildText);
+    const addChannelButton = new ButtonBuilder()
+      .setLabel("Adicionar canal")
+      .setCustomId("add-channel-allowed-button")
+      .setStyle(ButtonStyle.Success);
+
+    const backToHome = new ButtonBuilder()
+      .setLabel("Voltar ao início")
+      .setCustomId("back-to-home-button")
+      .setStyle(ButtonStyle.Secondary);
+    this.selectRow =
+      new ActionRowBuilder<ChannelSelectMenuBuilder>().addComponents(
+        selectChannel
+      );
+
+    this.buttonRow = new ActionRowBuilder<ButtonBuilder>().addComponents(
+      addChannelButton,
+      backToHome
+    );
+
+    await interaction.message.edit({
+      embeds: [this.embed],
+      components: [this.selectRow, this.buttonRow],
+    });
+  }
+
+  async editChannelSelected(interaction: ChannelSelectMenuInteraction) {
+    if (!this.embed || !this.selectRow || !this.buttonRow) return;
+
+    this.channelSelected = interaction.values[0];
+
+    await interaction.message.edit({
+      embeds: [
+        this.embed.setDescription(
+          `Selecione o canal aonde os IA Chats serão criados\n\nCanal selecionado: <#${this.channelSelected}>`
+        ),
+      ],
+      components: [this.selectRow, this.buttonRow],
+    });
+  }
+
+  async createPannel(interaction: ButtonInteraction) {
+    if (!this.channelSelected) return;
+
+    const channel = interaction.client.channels.cache.get(this.channelSelected);
+
+    if (!channel || !channel.isTextBased() || channel.isDMBased()) return;
+
+    const pannel = responseEmbed(
+      "Inicie seu Chat com a Deborah",
+      `
+      Para iniciar seu chat com a Deborah, clique no botão abaixo
+      Informações sobre o Chat:
+      - Encerre o chat depois que acabar de utilizar
+      \n- Nesse chat a Deborah tem memória, então você pode fazer perguntas relacionadas às mensagens anteriores
+      \n- Nesse Chat a Deborah dará respostas mais completas e aprofundadas, para respostas mais simples, use o /ask
+      \n- O chat é encerrado e todo o histórico é apagado automaticamente depois de 1 dia após ser criado
+      \n- O Chat também pode ser encerrado escrevendo !encerrar
+      `
+    );
+
+    const createChannel = new ButtonBuilder()
+      .setLabel("Iniciar chat")
+      .setCustomId("create-ai-chat")
+      .setStyle(ButtonStyle.Primary);
+
+    const buttonRow = new ActionRowBuilder<ButtonBuilder>().addComponents(
+      createChannel
+    );
+
+    channel.send({ embeds: [pannel], components: [buttonRow] });
+  }
+}
